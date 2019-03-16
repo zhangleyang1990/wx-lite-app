c65a1eec94c506ed6c704c76c775085c09760fb9
zhaogaoshang <zhaogaoshang@163.com>
2019-03-16 23:39:34 +0800

    初始

---
+++ lite-app/pages/index/index.js
@@ -0,0 +1,46 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    handSwiper: ['0.jpg', '1.jpg', '2.jpg', '3.png', '4.jpg'],
+    category: [{
+        name: '关于我们',
+        target: 'myAbout'
+      },
+      {
+        name: '相关资讯',
+        target: 'nweAbout'
+      },
+      {
+        name: '产品证书',
+        target: 'product'
+      },
+      {
+        name: '售后团队',
+        target: 'sellServer'
+      },
+    ]
+  },
+
+  handleGoOtherPage(e) {
+    console.log(e.currentTarget.dataset.type)
+    let target = e.currentTarget.dataset.type
+    wx.navigateTo({
+      url: '../' + target + '/' + target,
+    })
+  },
+
+  onLoad: function() {
+
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
